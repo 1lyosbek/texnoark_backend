c82238509519eeddfac4990e988bd3c202e77f2d
Ilyosbek <ilyosxonisaqov007@gmail.com>
2024-06-09 01:34:46 +0500

    updated

--- src/modules/stock/stock.controller.ts
+++ src/modules/stock/stock.controller.ts
@@ -2,33 +2,38 @@ import { Controller, Get, Post, Body, Patch, Param, Delete, ParseIntPipe, Inject
 import { StockService } from './stock.service';
 import { CreateStockDto } from './dto/create-stock.dto';
 import { UpdateStockDto } from './dto/update-stock.dto';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
 @ApiTags('stock')
 @Controller('stock')
 export class StockController {
   constructor(@Inject("IStockService") private readonly stockService: StockService) {}
 
+  @ApiOperation({summary: "Create new stock"})
   @Post('create')
   async create(@Body() createStockDto: CreateStockDto) {
     return await this.stockService.create(createStockDto);
   }
 
+  @ApiOperation({ summary: "Get all stocks" })
   @Get()
   async findAll() {
     return await this.stockService.findAll();
   }
 
+  @ApiOperation({ summary: "Get stock by id" })
   @Get(':id')
   async findOne(@Param('id', ParseIntPipe) id: number) {
     return await this.stockService.findOne(id);
   }
 
+  @ApiOperation({ summary: "Update stock by id" })
   @Patch('update/:id')
   async update(@Param('id', ParseIntPipe) id: number, @Body() updateStockDto: UpdateStockDto) {
     return await this.stockService.update(id, updateStockDto);
   }
 
+  @ApiOperation({ summary: "Delete stock by id" })
   @Delete('delete/:id')
   async remove(@Param('id', ParseIntPipe) id: number) {
     const {data: foundStock } = await this.stockService.findOne(id);
