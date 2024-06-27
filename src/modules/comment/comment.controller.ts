4487e74a4e8cb1fef74a866221c962bf6af34026
Ilyosbek <ilyosxonisaqov007@gmail.com>
2024-06-28 04:46:45 +0500

    updated

--- src/modules/comment/comment.controller.ts
+++ src/modules/comment/comment.controller.ts
@@ -4,12 +4,15 @@ import { CreateCommentDto } from './dto/create-comment.dto';
 import { CurrentUser } from 'src/common/decorator/CurrentUser.decorator';
 import { UserEntity } from '../user/entities/user.entity';
 import { ApiTags } from '@nestjs/swagger';
+import { Auth } from 'src/common/decorator/auth.decorator';
+import { RoleEnum } from 'src/common/enums/enums';
 
 @ApiTags('comments')
 @Controller('comment')
 export class CommentController {
   constructor(@Inject('ICommentService') private readonly commentService: CommentService) {}
 
+  @Auth(RoleEnum.ADMIN, RoleEnum.SUPERADMIN, RoleEnum.USER)
   @Post('create')
   create(@Body() createCommentDto: CreateCommentDto, @CurrentUser() currentUser: UserEntity) {
     return this.commentService.create(createCommentDto, currentUser);
--- src/modules/comment/comment.module.ts
+++ src/modules/comment/comment.module.ts
@@ -4,9 +4,10 @@ import { CommentController } from './comment.controller';
 import { CommentRepository } from './comment.repository';
 import { CommentEntity } from './entities/comment.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { SharedModule } from '../shared/shared.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([CommentEntity])],
+  imports: [TypeOrmModule.forFeature([CommentEntity]), SharedModule],
   controllers: [CommentController],
   providers: [
     {provide: "ICommentService", useClass: CommentService},
